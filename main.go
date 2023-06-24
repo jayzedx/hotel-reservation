2ff9cdece665d4f8e837a157fe79204309662622
jayzedx <d.chalermpong@gmail.com>
2023-06-25 00:27:03 +0700

    Adding database layer

--- main.go
+++ main.go
@@ -1,23 +1,64 @@
 package main
 
 import (
+	"context"
 	"flag"
+	"log"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/jayzedx/hotel-reservation/api"
+	"github.com/jayzedx/hotel-reservation/db"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
+const dburi = "mongodb://localhost:27017"
+const dbname = "hotel-reservation"
+const userColl = "users"
+
+var config = fiber.Config{
+	ErrorHandler: func(ctx *fiber.Ctx, err error) error {
+		return ctx.JSON(map[string]string{"error": err.Error()})
+	}}
+
 func main() {
+	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(dburi))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	/*
+		ctx := context.Background()
+		coll := client.Database(dbname).Collection(userColl)
+		user := types.User{
+			FirstName: "Jay",
+			LastName:  "Layman ",
+		}
+		_, err = coll.InsertOne(ctx, user)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		var jay types.User
+		if err := coll.FindOne(ctx, bson.M{}).Decode(&jay); err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println(jay)
+	*/
+
 	listenAddr := flag.String("listenAddr", ":5000", "The listen address of the API server")
 	flag.Parse()
 
-	app := fiber.New()
+	app := fiber.New(config)
 	apiv1 := app.Group("/api/v1")
 
 	app.Get("/foo", handleFoo)
 
-	apiv1.Get("/user", api.HandleGetUsers)
-	apiv1.Get("/user/:id", api.HandleGetUser)
+	//handlers initialization
+	userHandler := api.NewUserHandler(db.NewMongoUserStore(client))
+
+	apiv1.Get("/user", userHandler.HandleGetUsers)
+	apiv1.Get("/user/:id", userHandler.HandleGetUser)
 
 	app.Listen(*listenAddr)
 }
--- types/user.go
+++ types/user.go
@@ -1,7 +1,7 @@
 package types
 
 type User struct {
-	Id        string `bson:"_id" json:"id,omitempty"` //omitempty - don't show json when id is empty
+	Id        string `bson:"_id,omitempty" json:"id,omitempty"` //omitempty - don't show json when id is empty
 	FirstName string `bson:"firstName" json:"firstName"`
 	LastName  string `bson:"lastName" json:"lastName"`
 }
